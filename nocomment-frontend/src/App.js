dfd5d8a026027281ca087b2e60a1038b8fd2390c
Seokhyun0079 <seokhyun0324@gmail.com>
2020-09-03 23:55:55 +0900

    이메일인증 접근 수정　メール認証画面接近制御

--- nocomment-frontend/src/App.js
+++ nocomment-frontend/src/App.js
@@ -14,10 +14,15 @@ function App() {
   const { user } = useSelector(({ user }) => ({
     user: user.user,
   }));
-
   useEffect(() => {
     if (user !== null && !user.emailCheck) {
       history.push('/authCode');
+    }
+    //로그인하지 않았거나 이미 이메일인증한 상태에서 이메일 인증 화면으로 접근 시 메인화면으로 이동함
+    //ログインしてない、または、すでにメールアドレス認証を行った状態でメールアドレス認証画面に接近するとメイン画面に遷移する
+    let { pathname } = history.location;
+    if (pathname === '/authCode' && (user === null || user.emailCheck)) {
+      history.push('/');
     }
   }, [history, user]);
   return (
