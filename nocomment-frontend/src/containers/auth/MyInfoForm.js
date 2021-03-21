2f5cdede988442f97b77e7b535b635831c4cdb8d
Seokhyun0079 <seokhyun0324@gmail.com>
2021-03-21 20:37:10 +0900

    for source share

--- nocomment-frontend/src/containers/auth/MyInfoForm.js
+++ nocomment-frontend/src/containers/auth/MyInfoForm.js
@@ -1,51 +1,84 @@
-import { Avatar } from 'material-ui';
-import React from 'react';
-import { StyledInput } from '../../components/common/StyledTag';
-import palette from '../../lib/styles/palette';
+import React, { useEffect } from 'react';
+import Button from '../../components/common/Button';
+import { RoundInput } from '../../components/common/StyledTag';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, update } from '../../modules/auth';
 
-const MyInfoForm = ({ user }) => {
-  return (
-    <>
-      <h2>profile</h2>
-      <Avatar
+const MyInfoForm = () => {
+  const dispatch = useDispatch();
+  const { user, form } = useSelector(({ user, auth }) => ({
+    user: user.user,
+    form: auth.update,
+  }));
+  const onChange = (e) => {
+    const { value, name } = e.target;
+    dispatch(
+      changeField({
+        form: 'update',
+        key: name,
+        value,
+      }),
+    );
+  };
+  const onSumbit = (e) => {
+    e.preventDefault();
+    const { name } = form;
+    dispatch(
+      update({
+        name: name,
+      }),
+    );
+  };
+  useEffect(() => {
+    dispatch(initializeForm('update'));
+  }, [dispatch, user]);
+  if (user) {
+    return (
+      <div
         style={{
-          width: 200,
-          height: 200,
-          background: palette.yellow[0],
+          width: '50%',
+          marginLeft: 'auto',
+          marginRight: 'auto',
+          padding: '1rem',
         }}
-        src="/api/drawingComment/getImageFile/file-177c4d7f954.jpg"
-      ></Avatar>
-      <form>
-        <h4>아이디</h4>
-        <StyledInput
-          autoComplete="authCode"
-          name="stringId"
-          type="text"
-          value={user.stringId}
-          readOnly
-          disabled
-        />
-        <h4>이메일</h4>
-        <StyledInput
-          autoComplete="authCode"
-          name="stringId"
-          type="text"
-          value={user.email}
-          readOnly
-          disabled
-        />
-        <h4>유저명</h4>
-        <StyledInput
-          autoComplete="authCode"
-          name="stringId"
-          type="text"
-          value={user.username}
-          readOnly
-          disabled
-        />
-      </form>
-    </>
-  );
+      >
+        <h2>profile</h2>
+
+        <form>
+          <h4>아이디</h4>
+          <RoundInput
+            name="stringId"
+            type="text"
+            value={user.stringId}
+            readOnly
+            disabled
+          />
+          <h4>이메일</h4>
+          <RoundInput
+            name="stringId"
+            type="text"
+            value={user.email}
+            readOnly
+            disabled
+          />
+          <h4>유저명</h4>
+          <RoundInput name="name" type="text" onChange={onChange} />
+        </form>
+        <Button
+          style={{
+            marginTop: '1.5rem',
+            width: '100%',
+            height: '40px',
+            borderRadius: '10px',
+          }}
+          onClick={onSumbit}
+        >
+          프로필 수정
+        </Button>
+      </div>
+    );
+  }
+  return <>로그인 먼저 해주셈!</>;
 };
 
 export default MyInfoForm;
