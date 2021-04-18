5f1974e63ad8e84367ffdd0370f5d897123ba23a
Seokhyun0079 <seokhyun0324@gmail.com>
2021-04-18 18:02:47 +0900

    s3 test

--- nocomment-backend/src/common/awsS3Buket.js
+++ nocomment-backend/src/common/awsS3Buket.js
@@ -2,20 +2,20 @@ import aws from 'aws-sdk';
 import multer from '@koa/multer';
 import multerS3 from 'multer-s3';
 import { logger } from './log';
-aws.config.getCredentials(function (err) {
-  if (err) console.log(err.stack);
-  // credentials not loaded
-  else {
-    console.log('Access key:', aws.config.credentials.accessKeyId);
-  }
+import dotenv from 'dotenv';
+
+dotenv.config();
+const s3 = new aws.S3({
+  accessKeyId: process.env.AWS_KEY_ID, // user 만들면서 지급받은 키값
+  secretAccessKey: process.env.AWS_KEY,
+  region: 'ap-northeast-1',
 });
-const s3 = new aws.S3();
 
 export const drawingCommentMulter = multer({
   storage: multerS3({
     s3: s3, // s3만 써도 됩니다.
     acl: 'public-read',
-    bucket: 'nocommentbuket/drawingComment',
+    bucket: process.env.AWS_BUCKET_NAME + '/drawingComment',
     key: function (req, file, cb) {
       cb(null, Date.now().toString() + '.png');
     },
@@ -25,7 +25,7 @@ export const profileImageMulter = multer({
   storage: multerS3({
     s3: s3, // s3만 써도 됩니다.
     acl: 'public-read',
-    bucket: 'nocommentbuket/profileImage',
+    bucket: process.env.AWS_BUCKET_NAME + '/profileImage',
     key: function (req, file, cb) {
       cb(null, `${Date.now().toString(16)}${file.originalname}`);
     },
@@ -35,7 +35,7 @@ export const postImageMulter = multer({
   storage: multerS3({
     s3: s3, // s3만 써도 됩니다.
     acl: 'public-read',
-    bucket: 'nocommentbuket/postImage',
+    bucket: process.env.AWS_BUCKET_NAME + '/postImage',
     key: function (req, file, cb) {
       cb(null, `${Date.now().toString(16)}${file.originalname}`);
     },
