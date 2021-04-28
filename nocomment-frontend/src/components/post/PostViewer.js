b6eb2847d72ccd3afcae266d8373d4af1413975c
sohot8653 <zzzzgggg2222@hanmail.net>
2021-04-28 23:30:37 +0900

    [프로필 1차개발완료] -ㅋㅋㅋ sohot8653

--- nocomment-frontend/src/components/post/PostViewer.js
+++ nocomment-frontend/src/components/post/PostViewer.js
@@ -5,6 +5,7 @@ import Responsive from '../common/Responsive';
 import SubInfo from '../common/SubInfo';
 import Tags from '../common/Tags';
 import { Helmet } from 'react-helmet-async';
+import Avatar from "@material-ui/core/Avatar";
 
 const PostViewerBlock = styled(Responsive)`
   margin-top: 4rem;
@@ -26,7 +27,25 @@ const PostContent = styled.div`
   border-bottom: 1px solid ${palette.gray[2]};
 `;
 
+const InfoArea = styled.div`
+  display: flex;
+`;
+
+const TextInfoArea = styled.div`
+  width: 90%;
+`;
+
+const ImageInfoArea = styled.div`
+  margin-right: 2%;
+  margin-top: 1%;
+`;
+
+// 기본 프로필 이미지 경로
+const fallbackImage = "https://nocommentbuket.s3-ap-northeast-1.amazonaws.com/commonImage/defualt-profile-img.png"
+
 const PostViewer = ({ post, error, loading, actionButtons, ownPost }) => {
+
+
   // 에러 발생 시
   if (error) {
     if (error.response && error.response.status === 404) {
@@ -39,25 +58,42 @@ const PostViewer = ({ post, error, loading, actionButtons, ownPost }) => {
   if (loading || !post) {
     return null;
   }
+
   const { title, body, noCommenter, publishedDate, tags } = post;
+
+  const profileImage = noCommenter.profileImage ? noCommenter.profileImage : fallbackImage;
+
   return (
     <PostViewerBlock>
       <Helmet>
         <title>{title} NOCOMMENT</title>
       </Helmet>
       <PostHead>
-        <h1>{title}</h1>
-        <SubInfo
-          name={noCommenter.name}
-          stringId={noCommenter.stringId}
-          publishedDate={publishedDate}
-          hasMarginTop
-        />
-        <Tags tags={tags} />
+        <InfoArea>
+          <ImageInfoArea>
+            <Avatar
+              src={profileImage}
+              style={{
+                width: 95,
+                height: 95
+              }}
+            ></Avatar>
+          </ImageInfoArea>
+          <TextInfoArea>
+            <h1>{title}</h1>
+            <SubInfo
+              name={noCommenter.name}
+              stringId={noCommenter.stringId}
+              publishedDate={publishedDate}
+              hasMarginTop
+            />
+            <Tags tags={tags} />
+          </TextInfoArea>
+        </InfoArea>
       </PostHead>
-      {actionButtons}
+      { actionButtons}
       <PostContent dangerouslySetInnerHTML={{ __html: body }} />
-    </PostViewerBlock>
+    </PostViewerBlock >
   );
 };
 
